649571dffa1307795e7ff18ad21d8ae256aad4fa
famfron <camerone2458@gmail.com>
2021-11-30 18:37:01 -0700

    Style calculator and set up number and basic operations

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Functions for different operations of the calculator
+
 function add(num1, num2) {
   return (num1 + num2);
 };//End of addition function
@@ -8,20 +10,81 @@ function subtract(num1, num2) {
 
 function multiply(num1, num2) {
   return (num1 * num2);
-};//End of multiply
+};//End of multiplication function
 
 function divide(num1, num2) {
   return (num1 / num2);
-};//End of divide
+};//End of division function
 
 function operate(operator, num1, num2) {
-  if(operator == '+') {
+  if(operator == 'add') {
     return add(num1, num2);
-  } else if(operator == '-') {
+  } else if(operator == 'subtract') {
     return subtract(num1, num2);
-  } else if(operator == '*') {
+  } else if(operator == 'multiply') {
     return multiply(num1, num2);
-  } else if(operator == '/') {
+  } else if(operator == 'divide') {
     return divide(num1, num2);
   }//End of if(operator)
-}; //End of operate
+}; //End of operation function
+
+
+//Adds event listeners to the different elements by category
+const numberButtons = document.querySelectorAll('.nmbrBtns');
+const operatorButtons = document.querySelectorAll('.oprtrBtns');
+const clearButtons = document.querySelectorAll('.topBtns');
+const calculatorDisplay = document.querySelector('#screenTxt');
+let nextNumber = '';
+let operatorFunction = '';
+let clearFunction = '';
+
+let currentNumber = '';
+let firstNumber = '';
+let secondNumber = '';
+let solution = '';
+
+let firstOperatorFunction = '';
+let secondOperatorFunction = '';
+
+//Fills the screen and stores the value of the current number
+for (let btn of numberButtons) {
+  btn.addEventListener('click', function() {
+    nextNumber = btn.id;
+    if(currentNumber.length < 14) {
+      if(btn.id === 'decimal' && !currentNumber.includes('.')) {
+      currentNumber = currentNumber + '.';
+    } else if(btn.id !== 'decimal') {
+      currentNumber = currentNumber + nextNumber;
+    };
+    };
+    calculatorDisplay.innerHTML = currentNumber;
+  });
+};//End of numberButtons
+
+for (let btn of operatorButtons) {
+  btn.addEventListener('click', function() {
+    firstOperatorFunction = secondOperatorFunction;
+    secondOperatorFunction = btn.id;
+    if (firstNumber === '') {
+      firstNumber = currentNumber;
+      currentNumber = ''
+    } else if (secondNumber === '') {
+      secondNumber = currentNumber;
+      currentNumber = '';
+      solution = operate(firstOperatorFunction, parseInt(firstNumber), parseInt(secondNumber));
+      firstNumber = solution;
+      secondNumber = ''
+      currentNumber = ''
+      solution = ''
+      calculatorDisplay.innerHTML = firstNumber;
+    }
+    console.log(firstNumber, secondNumber, solution);
+  });
+};//End of operatorButtons
+
+for (let btn of clearButtons) {
+  btn.addEventListener('click', function() {
+    clearFunction = btn.id;
+    console.log(clearFunction);
+  });
+};//End of clearButtons
